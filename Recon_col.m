db9113977d51e0539d71d3bd0fb5da458e416822
Ye XUE <sallyandx@hotmail.com>
2021-04-10 21:21:09 +0800

    Delete Recon_col.m

--- Recon_col.m
+++
@@ -1,7 +0,0 @@
-function pm25_recon_sample_c = Recon_col(Sparse_code_c,A,num_b)
-rec_Y_l1 = Sparse_code_c;
-     tmp_l1 = sort(abs(rec_Y_l1 ),'descend');
-     tmp_l1 = tmp_l1(num_b);
-     rec_Y_l1 (abs(rec_Y_l1 )<tmp_l1)=0;
-     pm25_recon_sample_c = A*rec_Y_l1;
-     
